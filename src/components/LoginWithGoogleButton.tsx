c5d75575f34f0d36a7b430fb31715ed87236feb8
Faraz Aulia <hey@rub1.cc>
2025-04-03 09:27:32 +0700

    update login with google button styles

--- src/components/LoginWithGoogleButton.tsx
+++ src/components/LoginWithGoogleButton.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { createBrowserClient } from '@/utils/supabase'
+import { cn } from '@/utils/tailwind'
+import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export default function LoginWithGoogleButton() {
@@ -8,9 +10,6 @@ export default function LoginWithGoogleButton() {
 
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: {
-        redirectTo: `${window.location.origin}/api/auth/callback`,
-      },
     })
 
     if (error) {
@@ -23,8 +22,19 @@ export default function LoginWithGoogleButton() {
   return (
     <button
       onClick={signIn}
-      className="mb-2 rounded-md border border-foreground/20 px-4 py-2 text-foreground"
+      className={cn(
+        'mx-auto text-lg',
+        'rounded-full border border-gray-300 bg-white px-8 py-4 text-black hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
+        'flex items-center justify-center gap-4',
+      )}
     >
+      <Image
+        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+        alt="Google logo"
+        className="inline-block h-5 w-5"
+        width={20}
+        height={20}
+      />
       Login with Google
     </button>
   )
